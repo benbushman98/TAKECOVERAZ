47050adc479c9c2baa99828601ebbde10a874f0f
Benjamin Bushman <benbushman98@gmail.com>
2026-05-28 11:51:04 -0600

    fix: removed border on icons

--- src/components/home/BandFacts.tsx
+++ src/components/home/BandFacts.tsx
@@ -20,7 +20,7 @@ function StatCard({ Icon, value, label }: StatCardProps) {
       <Box sx={{ m: 2, borderRadius: 1, color: 'white', textAlign: 'center', width: 288 }}>
         <Box sx={{ display: 'flex', justifyContent: 'center' }}>
           <Box sx={{ width: 144, height: 144, border: '4px solid', borderColor: 'error.main', borderRadius: '50%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-            <Box sx={{ width: 112, height: 112, bgcolor: 'grey.800', borderRadius: '50%', display: 'flex', alignItems: 'center', justifyContent: 'center', overflow: 'hidden' }}>
+            <Box sx={{ width: 112, height: 112, bgcolor: 'grey.800', borderRadius: '50%', display: 'flex', alignItems: 'center', justifyContent: 'center', overflow: 'hidden', transform: 'translateZ(0)' }}>
               <Icon sx={{ fontSize: 64, color: 'white'}} />
             </Box>
           </Box>
